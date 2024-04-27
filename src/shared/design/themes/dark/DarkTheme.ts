e06939d5acd3074b12179b81bff149ab27bedf83
anton-patrushev <anton11patrushev@gmail.com>
2024-04-27 17:44:43 +0200

    feat: add themes definition 🎨 - v0 theme containing just colors - definition for `DarkTheme` 🌚 - definition for `LightTheme` 🌝

---
+++ src/shared/design/themes/dark/DarkTheme.ts
@@ -0,0 +1,11 @@
+import type { Theme } from "~/shared/design/themes/Theme.types";
+import { Colors } from "~/shared/design/tokens/Colors.consts";
+
+export const DarkTheme: Theme = {
+  colors: {
+    primary: Colors.midnightPurple,
+    secondary: Colors.lightPurple,
+    background: Colors.blackPearl,
+    text: Colors.swanWhite,
+  },
+};
---
+++ src/shared/design/themes/light/LightTheme.ts
@@ -0,0 +1,11 @@
+import type { Theme } from "~/shared/design/themes/Theme.types";
+import { Colors } from "~/shared/design/tokens/Colors.consts";
+
+export const LightTheme: Theme = {
+  colors: {
+    primary: Colors.yellow,
+    secondary: Colors.sand,
+    background: Colors.swanWhite,
+    text: Colors.blackPearl,
+  },
+};
