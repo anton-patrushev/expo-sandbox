e3d50a2f1de4f46b30b593141b6c7b717ce931be
anton-patrushev <anton11patrushev@gmail.com>
2024-04-27 17:37:35 +0200

    feat: add fonts tokens - fonts under `assets/` (not loaded yet) - fonts families - fonts sizes - still missing font weights — but do we even need it 🤔

---
+++ src/shared/design/tokens/Fonts.consts.ts
@@ -0,0 +1,25 @@
+export const FontFamilies = {
+  goli: {
+    light: "Goli-Light",
+    regular: "Goli-Regular",
+    medium: "Goli-Medium",
+    bold: "Goli-Bold",
+  },
+  helloMickeys: {
+    regular: "Hello Mickeys",
+  },
+  spaceMono: {
+    regular: "SpaceMono-Regular",
+  },
+};
+
+export const FontSizes = {
+  titleXL: 31,
+  titleL: 25,
+  title: 19,
+  titleS: 17,
+  body: 14,
+  caption: 13,
+  captionS: 12,
+  captionXS: 11,
+};
