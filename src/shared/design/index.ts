abe20da78267f51e71bdfad93df4cd70cfefdce0
anton-patrushev <anton11patrushev@gmail.com>
2024-04-27 21:09:24 +0200

    feat: re-export things from `DesignSystem` root folder

---
+++ src/shared/design/index.ts
@@ -0,0 +1,10 @@
+// tokens
+export * from "./tokens/Colors.consts";
+export * from "./tokens/Fonts.consts";
+export * from "./tokens/Sizes.consts";
+
+// context provider
+export {
+  DesignSystemProvider,
+  useTheme,
+} from "./contexts/DesignSystem.context";
